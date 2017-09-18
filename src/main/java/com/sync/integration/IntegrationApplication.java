162484f1a07f005cdac2516abc0edee20156a739
suryakantaNITR <pradhan.suryakanta1@gmail.com>
2017-09-18 13:19:23 +0530

    Add component scan for sync module

--- src/main/java/com/sync/integration/IntegrationApplication.java
+++ src/main/java/com/sync/integration/IntegrationApplication.java
@@ -2,8 +2,10 @@ package com.sync.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.sync")
 public class IntegrationApplication {
 
 	public static void main(String[] args) {
